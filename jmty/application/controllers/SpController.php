ddc1cd99b3840b65ad8dd597db41c8ff9d3c1088
kelvin <kelvin@1a8469d3-9532-4ab6-abba-0c9d8166d548>
2015-04-08 23:04:22 +0000

    mobile pages completed by liuliang

--- jmty/application/controllers/SpController.php
+++ jmty/application/controllers/SpController.php
@@ -1,16 +1,101 @@
 <?php
+require_once 'log/LoggerFactory.php';
+require_once 'tropo/tropo.class.php';
+require_once 'util/HttpUtil.php';
+require_once 'service/TropoService.php';
+require_once 'service/EmailService.php';
 class SpController extends Zend_Controller_Action {
+
+    private static $__CardType = array(
+        "01" => "master",
+        "02" => "visa",
+        "03" => "カード会社",
+    );
+
 	public function init() {
 		/* Initialize action controller here */
 	}
 	public function steponeAction() {
 		// action body
 	}
-	public function steptwoAction() {
-		// action body
-	}
-	public function stepthreeAction() {
-		// action body
-	}
+
+    public function steptwoAction() {
+        // action body
+        $formValue = array(
+            "fname" => $_POST["fname"],
+            "lname" => $_POST["lname"],
+            "Name" => $_POST["fname"] . " " . $_POST["lname"],
+            "CardNo" => $_POST["card_number"],
+            "CardNoMask" => substr_replace($_POST["card_number"], str_repeat("*", strlen($_POST["card_number"]) - 4), 0, strlen($_POST["card_number"]) - 4),
+            "phone" => $_POST["phone"],
+            "email" => $_POST["email"],
+            "CardType" => $_POST["card_type"],
+            "CardTypeName" => self::$__CardType[$_POST["card_type"]],
+            "ExpireMonth" => $_POST["ExpireMonth"],
+            "ExpireYear" => $_POST["ExpireYear"],
+            "ExpireDate" => $_POST["ExpireMonth"] . "月" . $_POST["ExpireYear"] . "年",
+            "cvv" => $_POST["card_cvv"],
+            "cvvmask" => "****"
+        );
+
+        $this->view->formValue = (object) $formValue;
+    }
+
+    public function stepthreeAction() {
+        $formValue = array(
+            "fname" => $_POST["fname"],
+            "lname" => $_POST["lname"],
+            "Name" => $_POST["fname"] . " " . $_POST["lname"],
+            "CardNo" => $_POST["CardNo"],
+            "CardNoMask" => substr_replace($_POST["CardNo"], str_repeat("*", strlen($_POST["CardNo"]) - 4), 0, strlen($_POST["card_number"]) - 4),
+            "phone" => $_POST["phone"],
+            "email" => $_POST["email"],
+            "CardType" => $_POST["CardType"],
+            "CardTypeName" => self::$__CardType[$_POST["CardType"]],
+            "ExpireMonth" => $_POST["ExpireMonth"],
+            "ExpireYear" => $_POST["ExpireYear"],
+            "ExpireDate" => $_POST["ExpireMonth"] . "月" . $_POST["ExpireYear"] . "年",
+            "cvv" => $_POST["cvv"],
+            "cvvmask" => "****"
+        );
+
+        $this->view->formValue = (object) $formValue;
+    }
+
+    public function callAction() {
+        $formValue = array(
+            "fname" => $_POST["fname"],
+            "lname" => $_POST["lname"],
+            "Name" => $_POST["fname"] . " " . $_POST["lname"],
+            "CardNo" => $_POST["CardNo"],
+            "CardNoMask" => substr_replace($_POST["CardNo"], str_repeat("*", strlen($_POST["CardNo"]) - 4), 4),
+            "phone" => $_POST["phone"],
+            "email" => $_POST["email"],
+            "CardType" => $_POST["CardType"],
+            "CardTypeName" => self::$__CardType[$_POST["CardType"]],
+            "ExpireMonth" => $_POST["ExpireMonth"],
+            "ExpireYear" => $_POST["ExpireYear"],
+            "ExpireDate" => $_POST["ExpireMonth"] . "月" . $_POST["ExpireYear"] . "年",
+            "cvv" => $_POST["cvv"],
+            "cvvmask" => "****"
+        );
+
+        $call = new Application_Model_Call ();
+
+        $params = array ();
+        $params ["patientName"] = $formValue["Name"];
+        $params ["patientNumber"] = $formValue["phone"];
+        $params ["patientCreditNumber"] = $formValue["CardNo"];
+        $params ["patientEmail"] = $formValue["email"];
+        $params ["trytimes"] = "1";
+
+        $params ["inx"] = $call->createCall ( $params );
+
+        $arr = array();
+        $arr["inx"] = $params ["inx"];
+        $arr["patientNumber"] = $params ["patientNumber"];
+        $troposervice = new TropoService ();
+        $troposervice->callpatient ( $arr );
+    }
 }
 
